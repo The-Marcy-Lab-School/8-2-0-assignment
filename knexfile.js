6f889965692abb72cc963badcd0cef7978d0c7fe
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2023-10-02 12:58:05 -0400

    fix names

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,8 @@ module.exports = {
       host: process.env.PG_HOST || '127.0.0.1',
       port: process.env.PG_PORT || 5432,
       user: process.env.PG_USER || 'postgres',
-      password: process.env.PG_PASS || 'postgres',
-      database: process.env.database || 'postgres',
+      password: process.env.PG_PASSWORD || 'postgres',
+      database: process.env.PG_DATABASE || 'postgres',
     },
   },
   test: {
@@ -15,8 +15,8 @@ module.exports = {
       host: process.env.PG_HOST || '127.0.0.1',
       port: process.env.PG_PORT || 5432,
       user: process.env.PG_USER || 'postgres',
-      password: process.env.PG_PASS || 'postgres',
-      database: process.env.database || 'postgres',
+      password: process.env.PG_PASSWORD || 'postgres',
+      database: process.env.PG_DATABASE || 'postgres',
     },
   },
 };
