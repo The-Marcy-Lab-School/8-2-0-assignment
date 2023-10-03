8368bc72d293149665ce4209f5996d804e1f0a88
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2023-10-03 13:45:14 -0400

    Update connection to use render.com

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,8 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
+      connectionString: process.env.PG_CONNECTION_STRING,
+      ssl: { rejectUnauthorized: false },
       host: process.env.PG_HOST || '127.0.0.1',
       port: process.env.PG_PORT || 5432,
       user: process.env.PG_USER || 'postgres',
